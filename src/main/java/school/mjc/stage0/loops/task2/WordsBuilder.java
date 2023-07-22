e8993a77f5e838e00552d8c37edc3e2d58f8d549
jeko <jemali.ergemlidze.1@btu.edu.ge>
2023-07-22 16:09:23 +0400

    kksf

--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.loops.task2;
 public class WordsBuilder {
     public static void main(String[] args) {
         WordsBuilder w = new WordsBuilder();
-        w.buildPhrase('a','b','c','d');
+        w.buildPhrase('a','b','c','d','k');
     }
     public void buildPhrase(char... chars) {
         String res = "";
