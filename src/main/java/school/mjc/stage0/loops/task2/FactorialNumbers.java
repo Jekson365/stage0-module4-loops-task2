0b37ec7881e838e52a0533906bc215dc57d58ce3
jeko <jemali.ergemlidze.1@btu.edu.ge>
2023-07-22 15:59:42 +0400

    k

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -1,10 +1,6 @@
 package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
-    public static void main(String[] args) {
-        FactorialNumbers f=  new FactorialNumbers();
-        f.printFactorialRow(5);
-    }
     public void printFactorialRow(int printToInclusive) {
         int i = 1;
         int facts = 1;
