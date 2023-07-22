bf0c893411c687dc2eb5622fc3a82b1eb69492cc
jeko <jemali.ergemlidze.1@btu.edu.ge>
2023-07-22 15:55:11 +0400

    k

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -1,6 +1,26 @@
 package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
+    public static boolean isPrime(int number) {
+        if (number < 2) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
     public void printPrimeNumbers(int printToInclusive) {
+        int start = 1;
+        int end = 10;
+
+        while (start <= end) {
+            if (isPrime(start)) {
+                System.out.println(start);
+            }
+            start++;
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -1,7 +1,18 @@
 package school.mjc.stage0.loops.task2;
 
 public class WordsBuilder {
+    public static void main(String[] args) {
+        WordsBuilder w = new WordsBuilder();
+        w.buildPhrase('a','b','c','d');
+    }
     public void buildPhrase(char... chars) {
+        String res = "";
+        int i = 0;
+        while (i < chars.length) {
+            res += chars[i];
+            i++;
+        }
 
+        System.out.println(res);
     }
 }
